b5a3e1063cd16db498b12f7c31957fb33f6f4e87
Burak Celik <burk.clik@gmail.com>
2019-02-16 15:09:21 +0300

    ignore

--- compliment.py
+++ compliment.py
@@ -1,10 +1,3 @@
-# try:
-#     num = int(input("Sayı : "))
-# except ValueError as value:
-#     print("Integer tipinde veri giriniz")
-# finally:
-#     print("The END")
-
 def compliment(num_of_compliment, num_s_compliment):
     if int(num_of_compliment) == 10:
         result = (10 ** len(str(num_s_compliment))) - int(num_s_compliment)
@@ -16,7 +9,3 @@ def compliment(num_of_compliment, num_s_compliment):
             top_of_num += num_of_compliment
         result = (int(top_of_num) - int(num_s_compliment))
         return result
-
-num = input("Hangi sayının tümleyeni : ")
-num2 = input("Sayı : ")
-print(compliment(num, num2))
