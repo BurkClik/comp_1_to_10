42d158e18358ab7304b28b5c0bcd26cb8bba997d
Burak Celik <burk.clik@gmail.com>
2019-02-18 15:44:15 +0300

    Decimal to Binary Func

--- Compliment/compliment.py
+++ Compliment/compliment.py
@@ -9,3 +9,20 @@ def compliment(num_of_compliment, num_s_compliment):
             top_of_num += num_of_compliment
         result = (int(top_of_num) - int(num_s_compliment))
         return result
+
+
+def decimal_to_binary(num):
+    binary = ""
+    div = 3
+    while(div >= 2):
+        div = num // 2
+        remainder = num % 2
+        print("num -> {}\ndiv -> {}\nremainder -> {}".format(num, div, remainder))
+        num = div
+        binary += str(remainder)
+        if div < 2:
+            binary += str(div)
+    return binary[::-1]
+
+num = int(input("Sayi : "))
+print(decimal_to_binary(num))
